fdcb20408ab6ac21da780eddff6883a4e8cae696
aavashneupane <aavashneupane4@gmail.com>
2021-03-24 22:08:54 +0545

    making style for navigation bar

---
+++ src/Header/MenuItems.js
@@ -0,0 +1,27 @@
+export const MenuItems=[
+    {
+        title:'Home',
+        url:'#',
+        cName:'nav-links'
+    },
+    {
+        title:'Sign up',
+        url:'#',
+        cName:'nav-links'
+    },
+    {
+        title:'Login',
+        url:'#',
+        cName:'nav-links'
+    },
+    {
+        title:'Add Job',
+        url:'#',
+        cName:'nav-links'
+    },
+    {
+        title:'Show all jobs',
+        url:'#',
+        cName:'nav-links-mobile'
+    },
+]
--- src/Header/header.js
+++ src/Header/header.js
@@ -1,18 +1,38 @@
-import {Link} from 'react-router-dom';
-const {Component}=require("react");
+import { MenuItems } from "./MenuItems";
+import { Link } from 'react-router-dom';
+const { Component } = require("react");
 
 
 class Header extends Component {
 
     render() {
-        return(
-            <div>
+        return (
+            <nav className="NavbarItems">
+                <h1 className="navbar-logo">React<i className="fa fa-react"></i></h1>
+                <div className="menu-icon">
+
+                </div>
+                <ul>
+                    {MenuItems.map((item, index) => {
+                        return (
+                            <li key={index}>
+                                <a className={item.cName} href={item.url}>
+                                {item.title}
+                                </a>
+                            </li>
+                        )
+                    })}
+
+
+                </ul>
+
+
                 <Link to="/register"> Register</Link>
                 <Link to="/login"> Login</Link>
                 <Link to="/jobAdd"> Job Add</Link>
                 <Link to="/updateJob"> Update Job</Link>
                 <Link to="/showAllJob"> Show All Job</Link>
-            </div>
+            </nav>
         )
     }
 

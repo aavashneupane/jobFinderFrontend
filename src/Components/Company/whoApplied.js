6e58ca383d188c2ea4a37049de316bbd787532c2
aavashneupane <aavashneupane4@gmail.com>
2021-03-30 01:39:39 +0545

    api to see which user applied for job. Testing left

---
+++ src/Components/Company/whoApplied.js
@@ -0,0 +1,100 @@
+import React, { Component, useState } from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
+import { Route, Link,useParams } from 'react-router-dom';
+import { Button } from "../../Header/Button";
+import { Card } from 'react-bootstrap';
+//import { axios } from 'axios';
+const axios = require('axios').default;
+
+
+
+
+class whoApplied extends Component {
+    
+    state = {
+        jobs: [],
+        config: {
+            headers: { 'authorization': `Bearer ${localStorage.getItem('token')}` }
+        },
+        id : this.props.match.params.id
+    }
+    
+    componentDidMount() {
+
+        axios({
+            method: 'get',
+            url: 'http://localhost:91/job/addshowWhoApplied/',
+            data: this.state.id,
+            headers : {'authorization': `Bearer ${localStorage.getItem('token')}`}
+          })        
+            //axios.get('http://localhost:91/job/showWhoApplied/',this.state.id)
+            .then((response) => {
+                console.log(response)
+                this.setState({
+                    jobs: response.data
+                })
+            })
+            .catch((err) => {
+                console.log(err.response);
+                alert("Error. Please Login first")
+            })
+    }
+
+    deleteJob = (id) => {
+        axios.delete('http://localhost:91/job/delete/' + id, this.state.config)
+            .then((response) => {
+                console.log(response)
+                alert("Delete successfull")
+            })
+            .catch((err) => {
+                //console.log(err.response)
+                alert("Delete unsuccessfull")
+            })
+
+    }
+    applyJob = (id) => {
+        axios({
+            method: 'post',
+            url: 'http://localhost:91/job/applyJob',
+            data: id,
+            headers : {'authorization': `Bearer ${localStorage.getItem('token')}`}
+          })
+          .then((response) => {
+                //             console.log(response)
+                alert("Job apply successfull")
+            })
+            .catch((err) => {
+                //console.log(err.response)
+                alert("Job apply unsuccessfull")
+            })
+
+    }
+    // updateJob = (e)=>{
+    //     e.preventDefault()
+    //     axios.put("http://localhost:90/job/update", this.state)
+    //     .then((response)=>{
+    //         console.log(response)
+    //         alert("Job update successfull")
+    //     })
+    //     .catch((err)=>{
+    //         console.log(err.response)
+    //         alert("Job update unsuccessfull")
+    //     })
+    // }
+
+
+    render() {
+        return (
+            <Container>
+                <Row>
+
+                    This is who applied page
+                    
+                </Row>
+            </Container>
+        )
+    }
+
+}
+
+export default whoApplied;
